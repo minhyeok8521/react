f058dee7f052dbb12319dd1f37b36a40569eba67
minhyeok8521 <124540446+minhyeok8521@users.noreply.github.com>
2023-07-18 10:31:59 +0900

    2 files modified

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <div className="header">
       <strong>헤더입니다.</strong>
+      <div className="headerMenu">
+        <Link to="/" className="homeBtn">
+          홈
+        </Link>
+      </div>
     </div>
   );
 }
