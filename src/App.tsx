ac7f66dc0398b367f71d202c8d1ef409fab846f0
minhyeok8521 <124540446+minhyeok8521@users.noreply.github.com>
2023-07-18 10:08:58 +0900

    1 file modified, 2 added, 2 deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './pages/Header/Header';
-import Footer from './pages/Footer/Footer';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import Main from './pages/main';
 import Product from './pages/Product';
 import NotFound from './pages/NotFound';
