5c11836eca2497feb50205cf5bf173450900be8e
minhyeok8521 <124540446+minhyeok8521@users.noreply.github.com>
2023-07-18 14:38:44 +0900

    2 files modified

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 import { useLocation } from 'react-router-dom';
 
 export default function Product() {
-  const product = useLocation();
+  const location = useLocation();
+  const product = location.state.productNumber;
   return (
     <>
       <h3>상품 페이지입니다.</h3>
-      <span>{product.state.abc}</span>
+      <span>{product}</span>
     </>
   );
 }
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -5,13 +5,7 @@ export default function Main() {
   return (
     <div>
       <h3>Main</h3>
-      <Link
-        to={{
-          pathname: './product',
-          search: '?number=1',
-          state: { abc: '1' },
-        }}
-      >
+      <Link to="./product/1" state={{ productNumber: '1' }}>
         1번 링크
       </Link>
       <Link to="./product/2">2번 링크</Link>
