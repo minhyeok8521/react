0757c07ce3a05d2d931b0e5b788c9e4376a0fee0
minhyeok8521 <124540446+minhyeok8521@users.noreply.github.com>
2023-07-20 14:05:59 +0900

    2 files modified, 1 added

--- src/components/Menu1/UserProfile.tsx
+++ src/components/Menu1/UserProfile.tsx
@@ -3,6 +3,8 @@ import * as React from 'react';
 class UserProfile extends React.Component {
   UserProfile = (e) => {
     e.preventDefault();
+
+    alert('a');
   };
 
   render() {
---
+++ src/components/Menu1/UserProfileList.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+class UserProfileList extends React.Component {
+  render() {
+    return (
+      <div className="userList">
+        <span>이름 : </span>
+        <span>나이 : </span>
+      </div>
+    );
+  }
+
+
+
+
+
+
+}
+
+export default UserProfileList;
